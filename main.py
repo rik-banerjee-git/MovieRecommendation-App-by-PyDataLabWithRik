d9312aa447afc395393bdea0c48d8dd19fe68922
rik-banerjee-git <riklearning13121999@gmail.com>
2025-01-26 19:33:00 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pickle
 import pandas as pd
 import requests
 from streamlit_lottie import st_lottie
+import gdown
 
 # Streamlit Page Configuration
 st.set_page_config(
@@ -18,8 +19,14 @@ with open("movie_data.pkl", "rb") as file:
 movie_data = pd.DataFrame.from_dict(loaded_data)
 
 # Load Precomputed Similarity Scores
-with open("similarity_score_precalculated.pkl", "rb") as file:
-    similarity_score_precalculated = pickle.load(file)
+# with open("similarity_score_precalculated.pkl", "rb") as file:
+#     similarity_score_precalculated = pickle.load(file)
+
+url = f'https://drive.google.com/uc?export=download&id=1zgfxuB_gau6HmLFD8N2lp5hlnHLgeJhs'
+#url='https://drive.google.com/file/d/1zgfxuB_gau6HmLFD8N2lp5hlnHLgeJhs/view?usp=sharing'
+gdown.download(url, 'similarity_score_precalculated.pkl', quiet=False)
+with open('similarity_score_precalculated.pkl', 'rb') as f:
+    similarity_score_precalculated = pickle.load(f)
 
 # TMDB API Call Function
 def tmdbApiCall(movieId):
